f94140484929941bed41157c7107a4947994c43b
Simi Adebowale <adebowalesimileoluwa@gmail.com>
2020-07-31 16:22:57 -0400

    Updates

---
+++ precache-manifest.df1241e25db885b294a247bb24d732c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088d6221f77f372b444cc3689dabf257",
+    "url": "/jokes-app/index.html"
+  },
+  {
+    "revision": "30b958181a657f06b96a",
+    "url": "/jokes-app/static/css/main.4b57e615.chunk.css"
+  },
+  {
+    "revision": "6ec0dc8c3f69845c638e",
+    "url": "/jokes-app/static/js/2.354a9919.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jokes-app/static/js/2.354a9919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b958181a657f06b96a",
+    "url": "/jokes-app/static/js/main.f5124b01.chunk.js"
+  },
+  {
+    "revision": "ebae275c1d737270c166",
+    "url": "/jokes-app/static/js/runtime-main.48a26c1e.js"
+  }
+]);
